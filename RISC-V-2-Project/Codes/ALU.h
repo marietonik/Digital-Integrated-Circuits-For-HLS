954e9bf5fcc91cd56a0c012663665d748a81e202
Maria Areti Germanou <53263761+marietonik@users.noreply.github.com>
2023-03-26 14:34:49 +0300

    backup

--- RISC-V-2-Project/Codes/ALU.h
+++ RISC-V-2-Project/Codes/ALU.h
@@ -75,5 +75,44 @@ class ALU
         ac_int<32,false> unsigned_op2 = op2;
         return unsigned_op1 >= unsigned_op2;
     }
+
+    ac_int<32,true> ALU::operations(ac_int<4,false> ALU_opcode, ac_int<32,true> operation_1, ac_int<32,true> operation_2, ){
+
+        switch(ALU_opcode){
+
+            case 1:
+                return ALU::add(operation_1, operation_2);
+            case 2:
+                return ALU::shift_left_logical(operation_1, operation_2);
+            case 3:
+                return ALU::signed_less_than(operation_1, operation_2);
+            case 4:
+                return ALU::unsigned_less_than(operation_1, operation_2);
+            case 5:
+                return ALU::logical_xor(operation_1, operation_2);
+            case 6:
+                return ALU::shift_right_logical(operation_1, operation_2);
+            case 7:
+                return ALU::logical_or(operation_1, operation_2);
+            case 8:
+                return ALU::logical_and(operation_1, operation_2);
+            case 9:
+                return ALU::mul(operation_1, operation_2);
+            case 10:
+                return ALU::sub(operation_1, operation_2);
+            case 11:
+                return ALU::shift_right_arithmetic(operation_1, operation_2);
+            case 12:
+                return ALU::comparator_equal(operation_1, operation_2);
+            case 13:
+                return ALU::not_equal(operation_1, operation_2);
+            case 14:
+                return ALU::signed_greater_than(operation_1, operation_2);
+            case 15:
+                return ALU::unsigned_greater_than(operation_1, operation_2);
+            default:
+                return 0;
+        }
+    }
 };
 #endif
