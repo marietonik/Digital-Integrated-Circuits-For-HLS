b3f9fabf86e75ad512dcccd54e74257f3804fa32
Maria Areti Germanou <53263761+marietonik@users.noreply.github.com>
2023-05-21 20:03:15 +0300

    main

--- RISC-V-2-Project/Codes/main.cpp
+++ RISC-V-2-Project/Codes/main.cpp
@@ -18,22 +18,25 @@ data_mem[100] = 100;
 data_mem[101] = 101; 
 
 // Test:
-instruction_mem[50] = 0b00000100011000000000001000010011; // ADDI R4, R0, 70
-instruction_mem[51] = 0b00000000010000100010001010000011; // LW R5, mem[ R4 + 1 ]
-instruction_mem[52] = 0b00000000011000100010010000100011; // SW   mem[r4 + 2] = R6
-/*instruction_mem[54] = 0b00000000000000000000000110010011; // ADDI R3, R0, 0
-instruction_mem[55] = 0b10101010100000011011001100110111; // LUI   R6, 10101010100000011011
-instruction_mem[56] = 0b00000000011000111111001110110011; // AND  R7, R7, R6
-instruction_mem[57] = 0b00000000000000000000001110010111; // AUIPC R7, 00000000000000000000
-instruction_mem[58] = 0b00000000010100100000110001100011; // BEQ  R4, R5, 24
-instruction_mem[65] = 0b00000000000000000000000000000000; // NOP
-*/
+
+instruction_mem[50] = 0b00000000100100011000010100110011; // ADD  R[10], R[3],  R[9]
+instruction_mem[51] = 0b01000000101001111000000010110011; // SUB  R[1],  R[15], R[10]
+instruction_mem[52] = 0b00000000001101001111001110110011; // AND  R[7],  R[9],  R[3]
+instruction_mem[53] = 0b00000000111100011110001110110011; // OR   R[7],  R[3],  R[15]
+instruction_mem[54] = 0b00000000100101010100010010110011; // XOR  R[9],  R[10], R[9]
+instruction_mem[55] = 0b00000000000100011010010100110011; // SLT  R[10], R[3],  R[1]
+instruction_mem[56] = 0b00000000000101111011000010110011; // SLTU R[1],  R[15], R[1]
+instruction_mem[57] = 0b01000000011100011101010010110011; // SRA  R[9],  R[3],  R[7]
+instruction_mem[58] = 0b00000000111100001101000110110011; // SRL  R[3],  R[1],  R[15]
+instruction_mem[59] = 0b00000001000000111001010100110011; // SLL  R[10], R[7],  R[16]
+instruction_mem[60] = 0b00000010111100111000000010110011; // MUL  R[1],  R[15], R[7]
+
+
 Processor processor;
 
 while (instruction_mem[i] != 0) {
 
 processor.run(instruction_mem, data_mem);
-
 i++;
 
 }
